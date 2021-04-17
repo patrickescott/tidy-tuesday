6df12c94f9ccc346cb9f53008bbef580ae78ce24
Patrick <patrickscott1712@gmail.com>
2021-04-17 21:16:42 +0100

    Getting rid of test markdown docs and adding markdown within my own script

--- 2021-04-12-post-offices/postal office locations.R
+++ 2021-04-12-post-offices/postal office locations.R
@@ -1,7 +1,20 @@
+#' ---
+#' title: "R scripts can be rendered!"
+#' author: "Jenny Bryan"
+#' date: "April 1, 2014"
+#' output: github_document
+#' ---
+#'
+#' In this initial section we're loading in the data.
+
+## load tidyverse
 library(tidyverse)
 
+## read data from TidyTuesday GitHub
 post_offices <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-04-13/post_offices.csv')
 
+#' With the data loaded in, I now want to loop through it to work out how many
+#' post office branches were open in a given year.
 open <- numeric(0)
 
 for (i in (1775:2002)) {
@@ -13,10 +26,12 @@ for (i in (1775:2002)) {
   open <- append(open, test)
 }
 
+## Create a tibble with the result of the loop
 openByYear <- tibble(year = 1775:2002, open = open)
+head(openByYear)
 
-# chart
-usps <- openByYear %>%
+#' Now we have our data we want to plot it with `ggplot2`
+openByYear %>%
   ggplot(aes(year, open/1000), group = 1, color = "#333466") +
   geom_area(alpha = 0.5, fill = "#333466") +
   geom_line() +
@@ -31,4 +46,4 @@ usps <- openByYear %>%
        subtitle = "Number of open USPS branches per year, thousands",
        caption = "Data: Cameron Blevins and Richard W. Helbock")
 
-ggsave("2020-04-12-post-offices/usps.png", usps, dpi = 144, width = 600/72, height = 338/72)
+## ggsave("2020-04-12-post-offices/usps.png", usps, dpi = 144, width = 600/72, height = 338/72)
